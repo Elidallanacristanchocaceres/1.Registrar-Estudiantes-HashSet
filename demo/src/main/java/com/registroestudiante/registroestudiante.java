b38ed4a87e6f7c3f307446934f1c6f4b189422a7
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-02-23 17:17:57 -0500

    feat: :construction: Se realizo la funcion para registrar estudiantes

--- demo/src/main/java/com/registroestudiante/registroestudiante.java
+++ demo/src/main/java/com/registroestudiante/registroestudiante.java
@@ -1,6 +1,17 @@
 package com.registroestudiante;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class registroestudiante {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<String> estudiantes = new HashSet<>();
+
+        // 1. Pedir la cantidad de estudiantes
+        System.out.print("Cuantos estudiantes quieres registrar: ");
+        int cantidad = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el buffer
     }
 }
